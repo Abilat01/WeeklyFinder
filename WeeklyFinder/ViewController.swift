e662231e97f0ab2e32834d010b96bb50f2ebbab6
Abilat01 <Abilat01@yandex.ru>
2021-09-16 18:55:51 +0300

    Initial Commit

---
+++ WeeklyFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeeklyFinder
+//
+//  Created by Danya on 16.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
