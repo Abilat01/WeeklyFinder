4710f7347d7f489a0e78c74e55180562a6c471cc
Abilat01 <Abilat01@yandex.ru>
2021-09-20 12:18:05 +0300

    Сделал на память

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var dayTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func findDay(_ sender: UIButton) {
+        
+        guard let day = dayTF.text, let month = monthTF.text, let year = yearTF.text else { return }
+        
+        let calendar = Calendar.current
+        var dateComponets = DateComponents()
+        dateComponets.day = Int(day)
+        dateComponets.month = Int(month)
+        dateComponets.year = Int(year)
+        
+        let dateFormater = DateFormatter()
+        dateFormater.locale = Locale(identifier: "ru_Ru")
+        dateFormater.dateFormat = "EEEE"
+        
+        guard let date = calendar.date(from: dateComponets) else { return }
+        
+        let weekday = dateFormater.string(from: date)
+        
+        resultLabel.text = weekday
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
